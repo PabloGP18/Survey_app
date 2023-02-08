247c6f5ffbaa3e0a30ed53186e3108643ab27634
PabloGP18 <garciaplaza.pablo@gmail.com>
2023-02-08 14:09:41 +0100

    fixed responsiveness

--- app/src/pages/ErrorPage.js
+++ app/src/pages/ErrorPage.js
@@ -3,12 +3,12 @@ import styles from './errorpage.module.scss'
 
 const ErrorPage = () => (
 	<div className={styles.error}>
-		<h1>Oops! Something went wrong.</h1>
-		<p>
+		<h1 className={styles.title_error}>Oops! Something went wrong.</h1>
+		<p className={styles.p_error}>
 			We&apos;re sorry, but an error has occurred while trying to load this
 			page.
 		</p>
-		<p>
+		<p className={styles.p_error}>
 			Please try again later or contact the site administrator if the problem
 			persists.
 		</p>
